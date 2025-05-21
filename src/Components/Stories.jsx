a41669e30d12df61f6a67fb89762c29bce29a2f0
irorere juliet <irorerejuliet@gmail.com>
2025-05-21 14:57:50 +0100

    make changes on stories

--- src/Components/Stories.jsx
+++ src/Components/Stories.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+
 import { Link } from 'react-router-dom';
 import TextEllipse from '../Components/TexEllipse'
 import { storiesData } from '../Constants/StoriesData';
+
 
 const Stories = () => {
   return (
